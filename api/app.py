a4ba79ee3afa86a9d54cda5302c3daa131ea3309
miztch <miztch.me@gmail.com>
2023-10-08 04:12:37 +0900

    refactor: separate logic for date validation

--- api/app.py
+++ api/app.py
@@ -1,7 +1,5 @@
-import re
-from datetime import datetime
 from chalice import Chalice
-from chalicelib import database
+from chalicelib import database, date_format
 
 app = Chalice(app_name="dima")
 
@@ -23,31 +21,15 @@ def matches():
     app.log.debug("Request: {}".format(request.to_dict()))
 
     # default date for search: today
-    date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+    date = date_format.get_default()
 
     if request.query_params:
         param = request.query_params.get("date")
 
         # validate date format of query parameter (YYYY-mm-dd)
-        # if invalid, parameter date -> today.
-        pattern = "^(?!([02468][1235679]|[13579][01345789])00-02-29)(([0-9]{4}-(01|03|05|07|08|10|12)-(0[1-9]|[12][0-9]|3[01]))|([0-9]{4}-(04|06|09|11)-(0[1-9]|[12][0-9]|30))|([0-9]{4}-02-(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}([02468][048]|[13579][26])-02-29))$"
-
-        try:
-            if re.fullmatch(pattern, param) is not None:
-                app.log.debug(
-                    "Date format in query parameter is valid. input: {}".format(param)
-                )
-                date = param
-            else:
-                app.log.debug(
-                    "Date format in query parameter is invalid or empty. input: {}".format(
-                        param
-                    )
-                )
-        except TypeError:
-            app.log.debug(
-                "query parameter with invalid format given. input: {}".format(param)
-            )
+        # if format is valid, overwrite date with parameter
+        if param and date_format.validate(param):
+            date = param
 
     matches = database.query(date)
     app.log.debug("Return {} items.".format(len(matches)))

b060aad8383bdd4de88459025a4e15059cbe2bb3
miztch <8962490+miztch@users.noreply.github.com>
2024-07-10 01:07:47 +0900

    fix: json key

--- match.go
+++ match.go
@@ -1,17 +1,17 @@
 package main
 
 type match struct {
-	Name             string `dynamodbabv:"matchName"`
-	StartTime        string `dynamodbabv:"startTime"`
-	EventName        string `dynamodbabv:"eventName"`
-	Teams            []team `dynamodbabv:"teams"`
-	StartDate        string `dynamodbabv:"startDate"`
-	PagePath         string `dynamodbabv:"pagePath"`
-	BestOf           int    `dynamodbabv:"bestOf"`
-	Id               int    `dynamodbabv:"id"`
-	EventCountryFlag string `dynamodbabv:"eventCountryFlag"`
+	Name             string `json:matchName dynamodbabv:"matchName"`
+	StartTime        string `json:startTime dynamodbabv:"startTime"`
+	EventName        string `json:eventName dynamodbabv:"eventName"`
+	Teams            []team `json:teams dynamodbabv:"teams"`
+	StartDate        string `json:startDate dynamodbabv:"startDate"`
+	PagePath         string `json:pagePath dynamodbabv:"pagePath"`
+	BestOf           int    `json:bestOf dynamodbabv:"bestOf"`
+	Id               int    `json:id dynamodbabv:"id"`
+	EventCountryFlag string `json:eventCountryFlag dynamodbabv:"eventCountryFlag"`
 }
 
 type team struct {
-	Title string
+	Title string `json:title`
 }
